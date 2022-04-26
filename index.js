4b7f66ead4374656cb301dc0bfa06112827af9e4
Ariful Islam Shuvo <arifulislams063@gmail.com>
2022-04-27 04:21:36 +0600

    created post api for keys

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -38,12 +39,22 @@ async function run() {
         products = await cursor.toArray();
         }
         res.send(products)
-    })
+    });
 
     //http://localhost:5000/productCount
     app.get('/productCount', async(req, res) => {
         const count = await productCollection.estimatedDocumentCount();
         res.send({count});
+    });
+    // use post to get products by ids
+    app.post('/productBykeys', async(req, res) => {
+        const keys = req.body;
+        const ids = keys.map(id => ObjectId(id));
+        const query = {_id: {$in: ids}};
+        const cursor = productCollection.find(query);
+        const products = await cursor.toArray(query)
+        console.log(keys);
+        res.send(products);
     })
   } finally {
 
